8a856af022d13fa06b47ab3ee9fece8a09ac5d1a
Lewis Harris <44181381+lewisharris@users.noreply.github.com>
2020-01-29 18:43:28 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,174 +1,55 @@
-var destinations = [
-    {city:'Hong Kong',
-    country:'',
-    continent:'asia',
-    weather:'hot',
-    environment:'city',
-    reason:'culture',
-    budget:'not-fussed',
-    image:'hong-kong'
-    },
-    {city:'Luang Prabang',
-    country:'Laos',
-    continent:'asia',
-    weather:'hot',
-    environment:'outdoors',
-    reason:'culture',
-    budget:'cheap',
-    image:'luang-prabang'
-    },
-    {city:'Phnom Penh',
-    country:'Cambodia',
-    continent:'asia',
-    weather:'hot',
-    environment:'city',
-    reason:'culture',
-    budget:'cheap',
-    image:'phnom-penh'
-    },
-    {city:'Penang',
-    country:'Malaysia',
-    continent:'asia',
-    weather:'hot',
-    environment:'city',
-    reason:'culture',
-    budget:'cheap',
-    image:'penang'
-    },
-    {city:'Singapore',
-    country:'',
-    continent:'asia',
-    weather:'hot',
-    environment:'city',
-    reason:'culture',
-    budget:'expensive',
-    image:'singapore'
-    },
-    {city:'Sydney',
-    country:'Australia',
-    continent:'pacific',
-    weather:'dont-care',
-    environment:'city',
-    reason:'culture',
-    budget:'expensive',
-    image:'sydney'
-    },
-    {city:'Hanoi',
-    country:'Vietnam',
-    continent:'asia',
-    weather:'dont-care',
-    environment:'city',
-    reason:'culture',
-    budget:'cheap',
-    image:'hanoi'
-    },
-    {city:'New York',
-    country:'USA',
-    continent:'America',
-    weather:'dont-care',
-    environment:'city',
-    reason:'culture',
-    budget:'expensive',
-    image:'new-york'
-    },
-    {city:'London',
-    country:'UK',
-    continent:'europe',
-    weather:'dont-care',
-    environment:'city',
-    reason:'culture',
-    budget:'expensive',
-    image:'london'
-    },
-    {city:'Tokyo',
-    country:'Japan',
-    continent:'asia',
-    weather:'dont-care',
-    environment:'city',
-    reason:'culture',
-    budget:'expensive',
-    image:'tokyo'
-    },
-    {city:'Rekyjavik',
-    country:'Iceland',
-    continent:'europe',
-    weather:'cold',
-    environment:'outdoors',
-    reason:'adventure',
-    budget:'expensive',
-    image:'rekyjavik'
-    },
-    {city:'Bangkok',
-    country:'Thailand',
-    continent:'asia',
-    weather:'hot',
-    environment:'city',
-    reason:'culture',
-    budget:'cheap',
-    image:'bangkok'
-    },
-    {city:'San Francisco',
-    country:'USA',
-    continent:'america',
-    weather:'dont-care',
-    environment:'city',
-    reason:'city',
-    budget:'expensive',
-    image:'san-francisco'
-    },
-    {city:'Queenstown',
-    country:'New Zealand',
-    continent:'pacific',
-    weather:'dont-care',
-    environment:'outdoors',
-    reason:'adventure',
-    budget:'expensive',
-    image:'queenstown'
-    },
-    {city:'Orlando',
-    country:'USA',
-    continent:'america',
-    weather:'hot',
-    environment:'outdoors',
-    reason:'adventure',
-    budget:'expensive',
-    image:'orlando'
+class Dest{
+    constructor(city,country,image){
+    this.city = city,
+    this.country = country,
+    this.image = image
     }
+}
+//add new destinations below
+var destinations = [
+new Dest('Hong Kong','','hong-kong'),
+new Dest('Luang Prabang','Laos','luang-prabang'),
+new Dest('Phnom Penh','Cambodia','phnom-penh'),
+new Dest('Penang','Malaysia','penang'),
+new Dest('Singapore','','singapore'),
+new Dest('Sydney','Australia','sydney'),
+new Dest('Hanoi','Vietnam','hanoi'),
+new Dest('New York','USA','new-york'),
+new Dest('London','UK','london'),
+new Dest('Reykjavik','Iceland','reykjavik'),
+new Dest('Toyko','Japan','tokyo'),
+new Dest('Bangkok','Thailand','bangkok'),
+new Dest('Hong Kong','USA','san-francisco'),
+new Dest('Queenstown','New Zealand','queenstown'),
+new Dest('Orlando','USA','orlando')
 ];
 
-var continentPicker = 'pacific';
-var find = document.getElementById('find');
-var landingFind = document.getElementById('landing-find');
-var results = document.getElementById('results');
-var result = document.getElementById('result');
-var image = document.getElementById('snapshot-container');
-var details = document.getElementById('details');
-var intro = document.getElementById('intro');
-var about = document.getElementById('about');
-var tryAgain = document.getElementById('try-again');
-
-// Country access
-var randomiser = Math.floor(Math.random()*destinations.length);
-var city = destinations[randomiser].city;
-var country = destinations[randomiser].country;
-
+function getId(e){return document.getElementById(e)} //id shortcut
+var find = getId('find');
+var results = getId('results');
+var result = getId('result');
+var image = getId('snapshot-container');
+var intro = getId('intro');
+var tryAgain = getId('try-again');
 
 function countryFinder(){
+    var randomiser = Math.floor(Math.random()*destinations.length);
+    var city = destinations[randomiser].city;
+    var country = destinations[randomiser].country;
     if(destinations[randomiser].country == ''){
         result.innerHTML = city.toUpperCase();
     }
     else{
         result.innerHTML = city.toUpperCase() + ', ' + destinations[randomiser].country;
     }
-    intro.style.display = 'none';
+    intro.className = 'hide';
     image.style.backgroundImage = "url('images/" + destinations[randomiser].image + ".jpeg')";
     results.style.opacity = 1;
     results.style.visibility = 'visible';
-    about.style.height = '100vh';
+    tryAgain.innerHTML = 'Don\'t fancy ' + city + '?';
 };
 
 
 
 find.addEventListener('click',countryFinder);
-
-tryAgain.innerHTML = 'Don\'t fancy ' + city + '?';
+tryAgain.addEventListener('click', countryFinder);
